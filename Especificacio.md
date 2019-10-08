5238a8959d0548d031d885f3a7386f4a0be0d30d
Miguel Paracuellos Ocana <miguel.paracuellos@c6s309pc62.fib.upc.es>
2019-10-08 11:58:33 +0200

    jajaj

---
+++ Especificacio.md
@@ -0,0 +1,16 @@
+# Especificació
+
+## Normes generals
+
+### Variables globals
+- Comencen amb majúscula. 
+	*(Variable_Global)*
+
+### Variables locals
+- Comencen amb minúscula.	
+	*(varLocal)*
+
+### Funcions
+- Comencen amb majúscula. 
+	*(FuncioAux)*
+
---
+++ proj/src/com/company/algorithm/LZW.java
@@ -0,0 +1,19 @@
+package com.company.algorithm;
+import java.util
+
+public class LZW {
+	
+	public static void compresser(String arxiu) {
+		Map<String,Integer> table = new HashMap<String, Integer>();
+		
+		for (int i = 0; i < 256; ++i) 
+			table[char(i)] = i;	
+			
+			
+		
+		
+	}
+
+
+	
+}
