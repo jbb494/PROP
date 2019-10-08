24d096e65940c1cc43754cab3ff562d42c62b969
mipaoc991 <miguelparacuellosocana@gmail.com>
2019-10-08 21:09:39 +0200

    LZW ja compila

--- proj/src/com/company/algorithm/LZW.java
+++ proj/src/com/company/algorithm/LZW.java
@@ -1,13 +1,16 @@
 package com.company.algorithm;
-import java.util
+import java.util.*;
 
 public class LZW {
 	
 	public static void compresser(String arxiu) {
 		Map<String,Integer> table = new HashMap<String, Integer>();
 		
-		for (int i = 0; i < 256; ++i) 
-			table[char(i)] = i;	
+		for (int i = 0; i < 256; ++i) {
+			table.put("" + (char)i, i);
+		}
+
+
 			
 			
 		
