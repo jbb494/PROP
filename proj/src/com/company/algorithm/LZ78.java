b10c28d791a5eb5fb7ea998ff4e023c57afcdeb9
Joan Bellavista Bartroli <joan.bellavista.bartroli@c6s309pc63.fib.upc.es>
2019-10-08 11:58:22 +0200

    Z78

---
+++ proj/src/com/company/algorithm/LZ78.java
@@ -0,0 +1,50 @@
+package com.company.algorithm;
+
+import java.util.*;
+
+
+public class LZ78
+{
+    String Input;
+    String Output;
+    
+    
+	public LZ78(String a) 
+	{
+        Map<Integer, Character> map = new HashMap<Integer, Character>();
+        
+		Input = a;
+		
+		Output = "";
+		
+		char nextChar;
+		
+		int i = 0;
+		
+		String aux = "";
+		
+		while(i < a.length())
+		{
+            nextChar = a.charAt(i);
+            
+            aux = aux.concat(Character.toString(nextChar));
+            
+            if(map.containsKey(nextChar))
+            {
+                
+            }else
+            {
+                map.get(aux);
+                
+            }
+            
+            i++;
+		}
+	}
+	
+	public String print()
+	{
+        return Output;
+	}
+	
+}
--- proj/src/main.java
+++ proj/src/main.java
@@ -1,13 +1,22 @@
 import com.company.input.Input;
-
+import com.company.algorithm.LZ78;
 
 public class Main {
 
     public static void main(String[] args) 
     {
         Input instance = new Input("Hello.txt");
+        
+        String aux = instance.getString();
 
-        System.out.println("Main: " + instance.getString());
+        System.out.println("Inicial: \n:" + aux);
+        
+        LZ78 alg = new LZ78(aux);
+        
+        aux = alg.print();
+        
+        System.out.println("Final: \n:" + aux);
+        
     }
 
 }
