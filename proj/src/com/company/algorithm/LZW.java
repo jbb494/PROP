e625d3d6e2f0d4d16a0442bd9c028b47fb5ef457
mipaoc991 <miguelparacuellosocana@gmail.com>
2019-10-09 14:20:03 +0200

    LZW updated

--- proj/src/com/company/algorithm/LZW.java
+++ proj/src/com/company/algorithm/LZW.java
@@ -3,13 +3,28 @@ import java.util.*;
 
 public class LZW {
 	
-	public static void compresser(String arxiu) {
+	public static void compresser(String file) {
 		Map<String,Integer> table = new HashMap<String, Integer>();
 		
 		for (int i = 0; i < 256; ++i) {
 			table.put("" + (char)i, i);
 		}
+		
+		Integer pos = 256;
+		String old = "";
+		for (char new_c : file.toCharArray()) {
+			String aux = old + new_c;
+			
+			if (table.containsKey(aux)) {
+				old = aux;
+			}
+			else {
+				table.put(aux, pos);
+				old = "" + new_c;
+				++pos;
+			}
 
+		}
 
 			
 			
