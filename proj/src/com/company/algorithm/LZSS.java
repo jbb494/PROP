e5b1796d2b752088f32465cf74e089f727d1f56a
Manel <manelaguilar@outlook.com>
2019-10-09 21:26:29 +0200

    Actualització

--- proj/src/com/company/algorithm/LZSS.java
+++ proj/src/com/company/algorithm/LZSS.java
@@ -5,68 +5,76 @@ import java.util.*;
 public class LZSS
 {
     static String Output;
-    public ArrayList<Integer> getKey(Map<K, V> map, V value) {
+
+    public ArrayList<Integer> GetKey(Map<Integer, Character> map, char value) {
         
         ArrayList<Integer> aux = new ArrayList<Integer>();
-        for (Entry<K, V> entry : map.entrySet())
+        for (Map.Entry<Integer, Character> entry : map.entrySet())
              if (entry.getValue().equals(value))
                  aux.add(entry.getKey());
         return aux;
     }
     
-    public static void compresser(String file)
+    public LZSS(String file)
     {
-        String out = "", aux = "";
-        int punter, paux;
-        Map<Integer, Character> vc = new TreeMap<Integer, Character>();
-        char nextChar;
-        for(int i = 0; i < file.size(); i++)
-        {
-            punter = i;
-            nextChar = file.charAt(i);
-            
-            if(vc.containsValue(nextChar))
-            {
-                aux.concat(String.valueOf(nextChar));
-                if(aux != "")
-                {
-                    ArrayList<Integer> keys = vc.getKey(aux.substring(0, 1));
-                    boolean found = false;
-                    for(int j = 0; j < keys.size() && !found; j++)
-                    {
-                        int ji = 0;
-                        int pivot = keys.get(j);
-                        while(!found && aux.charAt(ji) == vc.get(pivot + ji))
-                        {
-                            ji++;
-                            if(ji == aux.size()) found = true;
-                        }
-                    }
-                    if(!found && aux.size() >= 3)
-                    {
-                        aux = String.valueOf(nextChar);
-                        out.concat("COMPRESSED");
-                    }                    
-                }
-                vc.put(i, nextChar);
-            }
-            else
-            {
-                if(aux != "")
-                {
-                    if(aux.size() >= 3) out.concat("COMPRESSED");
-                    else out.concat(aux);
-                    aux = "";
-                }
-                else out.concat(String.valueOf(nextChar));
-                vc.put(i, nextChar);
-            }
-        }
-        Output = out;
+        System.out.println("Rata");
+        Output = compresser(file);
     }
+
     public String print()
     {
         return Output;
     }
 
+    public String compresser(String file)
+    {
+        String out = "", aux = "";
+        Map<Integer, Character> vc = new TreeMap<Integer, Character>();
+        char nextChar;
+        for (int i = 0; i < file.length(); i++)
+        {
+            nextChar = file.charAt(i);
+            System.out.println(nextChar);
+            aux.concat(String.valueOf(nextChar));
+            if(vc.containsValue(nextChar))
+            {
+                ArrayList<Integer> keys = GetKey(vc, aux.charAt(0));
+                boolean found = false;
+                for(int j = 0; j < keys.size() && !found; j++)
+                {
+                    int ji = 0;
+                    int pivot = keys.get(j);
+                    while(!found && aux.charAt(ji) == vc.get(pivot + ji))
+                    {
+                        ji++;
+                        if(ji == aux.length()) found = true;
+                    }
+                }
+                if(!found && aux.length() >= 3)
+                {
+                    aux = String.valueOf(nextChar);
+                    out.concat("COMPRESSED");
+                }
+            }
+            else
+            {
+                if(aux != "")
+                {
+                    if(aux.length() >= 3)
+                    {
+                        out.concat("COMPRESSED");
+                        aux = String.valueOf(nextChar);
+                    }
+                    else
+                    {
+                        out.concat(aux);
+                        aux = "";
+                    }
+                }
+                else out.concat(String.valueOf(nextChar));
+            }
+            vc.put(i, nextChar);
+        }
+        return out;
+    }
 }
