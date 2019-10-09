95a049462d315fec7624a19da12b89b9e77c1962
mipaoc991 <miguelparacuellosocana@gmail.com>
2019-10-09 15:14:20 +0200

    LZW: Compresser done

--- proj/src/com/company/algorithm/LZW.java
+++ proj/src/com/company/algorithm/LZW.java
@@ -3,15 +3,19 @@ import java.util.*;
 
 public class LZW {
 	
-	public static void compresser(String file) {
+	public static ArrayList<Integer> compresser(String file) {
 		Map<String,Integer> table = new HashMap<String, Integer>();
 		
+		//We initialize the map with the ASCII table 
 		for (int i = 0; i < 256; ++i) {
 			table.put("" + (char)i, i);
 		}
 		
+		//We prepare the variables before the main loop
 		Integer pos = 255;
 		String old = "";
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		
 		for (char new_c : file.toCharArray()) {
 			String aux = old + new_c;
 			
@@ -19,15 +23,22 @@ public class LZW {
 				old = aux;
 			}
 			else {
-				table.put(aux, ++pos);
+				//We add the next value to the compressed_file
+				result.add(pos);
+
+				//We add the word to the table
+				table.put(aux, pos++);
+
+				//We update the old value
 				old = "" + new_c;
 			}
-
+		}
+		
+		if (!old.equals("")) {
+			result.add(table.get(old));
 		}
 
-			
-			
-		
+		return result;
 		
 	}
 
