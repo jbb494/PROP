0598f355fdb6ee973c985b3cc4d540d8a8c153ca
jlapeyra <jlapeyraa@gmail.com>
2019-10-10 18:59:19 +0200

    JPEG: 1&3

--- proj/src/com/company/algorithm/JPEG.java
+++ proj/src/com/company/algorithm/JPEG.java
@@ -2,25 +2,44 @@
 
 
 public class JPEG {
-	public byte[][][] convert(byte inRGB[][][]) {
+	private void f(double[][] mat) {
+		mat[0][0] = 1.0;
+	}
+
+	public byte[][][] convert(byte[][][] inRGB) {
 		int n = inRGB.length;
-		int m = inRGB.[0].length;
-		byte inYCbCr[n][m][3];
+		int m = inRGB[0].length;
+		
+		//1. Color space transformation
+		double[][][] YCbCr = new double[3][n][m];
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < m; ++j) {
-				byte r, g, b;
+				double r, g, b;
 				r = inRGB[i][j][0]; g = inRGB[i][j][1]; b = inRGB[i][j][2];
-				byte y, cb, cr;
+				double y, cb, cr;
 				// https://sistenix.com/rgb2ycbcr.html
-				 
+				y = 16 + (65.738*r + 129.057*g + 25.064f*b)/256;
+				cb = 128 + (-37.945*r - 74.494*g + 112.439*b)/256;
+				cr = 128 + (112.439*r - 94.154*g - 18.285*b)/256;
+				YCbCr[0][i][j] = y; YCbCr[1][i][j] = cb; YCbCr[2][i][j] = cr;
 			}
 		}
+		
+		for (int k = 0; k < 3; ++k) {
+
+			//3. Block splitting
+			for (int i = 0; i+7 < n; i += 8) {
+				for (int j = 0; j+7 < m; j += 8) {
+					//f();
+				}
+			}
+		}
+
+		return inRGB;
 	}
 	
-	/*public static void main(String args[]) {
-			int a[][] = {{1, 2}, {3}};
-			for (int i[] : a) System.out.println(i.length);
-	}*/
+	public static void main(String args[]) {
+			
+	}
 }
 
-///github
