3c615943d4ec9fd014c48c9e71b0434a12fbe9c9
Joan <jbb494@gmail.com>
2019-10-12 13:25:26 +0200

    script -remove pels .class

--- proj/src/compileAndRun.sh
+++ proj/src/compileAndRun.sh
@@ -1,9 +1,14 @@
-ArxiusAcompilar=$(find | grep .java | sed "/LZW/ d" | sed "/JPEG/ d" | tr "\n" " ")
 
-echo $ArxiusAcompilar | sed "s/ /\n/g"
+if [[ $1 != "-remove" ]]; then
+    ArxiusAcompilar=$(find | grep .java | tr "\n" " ")
+    echo $ArxiusAcompilar | sed "s/ /\n/g"
+    javac $ArxiusAcompilar
+else
+    ArxiusAborrar=$(find | grep .class | tr "\n" " ")
+    echo $ArxiusAborrar | sed "s/ /\n/g"
+    rm $ArxiusAborrar
+fi
 
-javac $ArxiusAcompilar
-
-if [[ $1 == "-r" ]]; then
+if [[ $1 == "-run" || $1 == "-r" ]]; then
     java Main
 fi
