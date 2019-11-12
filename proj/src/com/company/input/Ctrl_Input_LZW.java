b7c681def17500a8d580e93320d7ba88dd1f9cfa
mipaoc991 <miguelparacuellosocana@gmail.com>
2019-11-12 17:16:46 +0100

    Doxygen done

--- proj/src/com/company/input/Ctrl_Input_LZW.java
+++ proj/src/com/company/input/Ctrl_Input_LZW.java
@@ -1,16 +1,29 @@
+/**
+ * @class Ctrl_Input_LZW
+ * @brief Accés a un arxiu comprimit amb LZW
+ * @author Miguel Paracuellos Ocaña
+ */
+
 package com.company.input;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.company.utils.byteToConversion;
 
 public class Ctrl_Input_LZW extends Ctrl_Input {
 
+    /**
+     * @brief Constructor de la classe
+     * @param path Path de l'arxiu comprimit
+     */
     public Ctrl_Input_LZW(String path) {
         super(path);
     }
 
+    /**
+     * @fn public Integer get()
+     * @return Primer enter no llegit de l'arxiu comprimit amb LZW
+     */
     public Integer get() {
         //ATM los valores codificados son de 4 bytes, en un futuro serÃ¡n de tamaÃ±o variable
         //y por lo tanto tendremos que tratarlos de la manera optima.
