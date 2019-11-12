e9e75ef692ba3efd7a5e5467e4d5924dd2eb6c79
Joan <joan.bellavista.bartroli@est.fib.upc.edu>
2019-11-13 00:11:54 +0100

    Doxygen

--- proj/src/com/company/algorithm/LZ78.java
+++ proj/src/com/company/algorithm/LZ78.java
@@ -1,13 +1,28 @@
+/**
+ * @class Algoritme LZ78
+ * @brief Compressió i descompressió pel mètode LZ78
+ * @author Joan Bellavista Bartroli
+ */
+
 package com.company.algorithm;
 import com.company.input.*;
 import com.company.output.Ctrl_Output;
+import com.company.utils.*;
 
 import java.util.*;
 
 
 public class LZ78 implements Algorithm {
+	/**
+     * @param Output Utilitzat per a la compressió i descompressió de fitxers.
+     */
     Ctrl_Output Output;
    
+	//Constructor
+	/**
+	 * @brief Constructor de la clase LZW
+	 * @param x Si es True, estarem comprimint, en cas contrari estarem descomprimint
+	 */
 	public LZ78(Boolean x){
 		String path;
 		if(x) path = "../DescompresioLZ78.out";
@@ -15,6 +30,13 @@ public class LZ78 implements Algorithm {
 		
 		Output = new Ctrl_Output(path);
 	}
+
+	//Functions
+	/**
+	 * @fn public Compressor(Ctrl_Input_Text in)
+	 * @brief Comprimim un text amb l'algoritme LZ78
+	 * @param in accés al Controlador d'Input per el text
+	 */
 	public void Compressor(Ctrl_Input_Text in)
 	{
         Map<String, Integer> map = new HashMap<String, Integer>();
@@ -63,25 +85,31 @@ public class LZ78 implements Algorithm {
 			}
 		}
 	}
+
+	/**
+	 * @fn public Decompressor(Ctrl_Input_LZW inp)
+	 * @brief Descomprimim un fitxer amb l'algoritme LZ78
+	 * @param inp accés al Controlador d'Input pel fitxer comprimit
+	 */
 	public void Decompressor(Ctrl_Input_LZ78 in) 
 	{
 		Map<Integer, String> map = new HashMap<Integer, String>();
 		Integer punterActual = 0;
-			while(!in.finished()) {
-				//System.out.println("Int: " + entr.getKey() + "Char: " + entr.getValue());
-				AbstractMap.SimpleEntry <Integer, Character> entr = in.get();
-				if(in.finished()) return;
-				Integer punterMap = entr.getKey();
-				Character nextChar = entr.getValue();
-				if(punterMap == 0){
-					Output.add(nextChar);
-					map.put(punterActual++, nextChar.toString());
-				}else {
-					String seqPunterMap = map.get(punterMap-1);
-					map.put(punterActual++, seqPunterMap.concat(nextChar.toString()));
-					Output.add(seqPunterMap.concat(nextChar.toString()));		
-				}
+		while(!in.finished()) {
+			//System.out.println("Int: " + entr.getKey() + "Char: " + entr.getValue());
+			Pair <Integer, Character> entr = in.get();
+			if(in.finished()) return;
+			Integer punterMap = entr.getLeft();
+			Character nextChar = entr.getRight();
+			if(punterMap == 0){
+				Output.add(nextChar);
+				map.put(punterActual++, nextChar.toString());
+			}else {
+				String seqPunterMap = map.get(punterMap-1);
+				map.put(punterActual++, seqPunterMap.concat(nextChar.toString()));
+				Output.add(seqPunterMap.concat(nextChar.toString()));		
 			}
+		}
 	}
 
 	public Ctrl_Output print()
--- proj/src/com/company/input/Ctrl_Input_LZ78.java
+++ proj/src/com/company/input/Ctrl_Input_LZ78.java
@@ -1,24 +1,38 @@
+/**
+ * @class Ctrl_Input_LZ78
+ * @brief Acces a un arxiu comprimit amb LZ78
+ * @author Joan Bellavista Bartroli
+ */
+
 package com.company.input;
 
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 import com.company.utils.byteToConversion;
-
+import com.company.utils.*;
 
 public class Ctrl_Input_LZ78 extends Ctrl_Input {
 
+    /**
+     * @brief Constructor de la classe
+     * @param path Path de l'arxiu comprimit
+     */
     public Ctrl_Input_LZ78(String path)
     {
         super(path);
     }
 
-    public AbstractMap.SimpleEntry <Integer, Character> get()
+    /**
+     * @fn public Pair<Integer, Character> get()
+     * @return Primer enter no llegit de l'arxiu comprimit amb LZ78
+     */
+    public Pair <Integer, Character> get()
     {
         Integer intAux = byteToConversion.byteToInteger(Input_class.getMoreBits(32));
 
         Character charAux = (char)Input_class.getBits(8);
 
-        return new AbstractMap.SimpleEntry <Integer, Character>(intAux, charAux);
+        return new Pair <Integer, Character>(intAux, charAux);
     }
 }
