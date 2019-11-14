02d9e38983c0af47179f543d4dc8690ee51661c2
Joan <joan.bellavista.bartroli@est.fib.upc.edu>
2019-11-14 22:37:45 +0100

    Drivers added

--- proj/src/compileAndRun.sh
+++ proj/src/compileAndRun.sh
@@ -1,9 +1,14 @@
 
-if [[ $1 != "-remove" || $1 == "-clean" ]]; then
+if [[ $1 != "-remove" && $1 != "-clean" && $1 != "-driver" ]]; then
     ArxiusAcompilar=$(find | grep .java | tr "\n" " ")
     echo $ArxiusAcompilar | sed "s/ /\n/g"
     javac $ArxiusAcompilar -d ../bin
     cp -r ../bin/* ../'drivers y tests exe'
+fi
+if [[ $1 == "-driver" ]]; then
+    cd ../'drivers y tests exe'
+    java $2
+    cd ../src
 fi
 
 if [[ $1 == "-run" || $1 == "-r" ]]; then
