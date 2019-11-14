d5010b26e9ea94b447d0d0b3396034ab10318f2a
mipaoc991 <miguelparacuellosocana@gmail.com>
2019-11-14 01:07:43 +0100

    Wortking

--- proj/src/persistencia/input/Ctrl_Input_Text.java
+++ proj/src/persistencia/input/Ctrl_Input_Text.java
@@ -1,19 +1,43 @@
 package persistencia.input;
 
+import domini.utils.utf8;
+
 public class Ctrl_Input_Text extends Ctrl_Input {
 
-    Byte seguent;
+    byte seguent;
 
     public Ctrl_Input_Text(String path) {
         super(path);
         seguent = Input_class.getBits(8);
     }
  
-
-    public char get() {
+    //Passa tots els caràcters a codi ASCII
+    public String get() {
         byte actual = seguent;
-        seguent = Input_class.getBits(8); 
-        return (char)actual;
+        seguent = Input_class.getBits(8);
+        String res = "";
+        char c = (char)seguent;
+        int i = (int)seguent; 
+        if (i < 128) {
+            res.concat((char) i + "");
+        } else if (i > 127 && i < 2048) {
+            int j = (i >> 6) | 192;
+            res += ((char) j);
+            j = (i & 63) | 128;
+            res += ((char) j);
+        } else {
+            int j = (i >> 12) | 224;
+            res += ((char) j);
+            j = ((i >> 6) & 63) | 128;
+            res += ((char) j);
+            j = (c & 63) | 128;
+            res += ((char) j);
+        }
+
+        char aux = (char)actual;
+        String ret = utf8.uft8Encode(aux + "");
+        return ret;
     }
+
 
 }
