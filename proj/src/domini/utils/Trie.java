3fbd7b0c2c512e9bb05cc055198d6d388f5424e7
mipaoc991 <miguelparacuellosocana@gmail.com>
2019-11-15 02:20:09 +0100

    Acabar doxygen

--- proj/src/domini/utils/Trie.java
+++ proj/src/domini/utils/Trie.java
@@ -1,14 +1,32 @@
+/**
+ * @class Trie
+ * @brief Classe de Trie
+ * @author Joan Bellavista
+ */
 package domini.utils;
 
 import java.util.ArrayList;
 
 public class Trie<T> {
+
+    /**
+     * @param root Arrel de l'arbre
+     */
     private TrieNode<T> root;
 
+    /**
+     * @brief Constructor de la classe
+     */
     public Trie() {
         root = new TrieNode<T>(-1);
     }
 
+    /**
+     * @fn public void insert(ArrayList<T> word, Integer index)
+     * @brief 
+     * @param word
+     * @param index
+     */
     public void insert(ArrayList<T> word, Integer index) {
         TrieNode<T> current = root;
 
@@ -19,6 +37,12 @@ public class Trie<T> {
         }
     }
 
+    /**
+     * @fn public Integer indexNode(ArrayList<T> word)
+     * @brief 
+     * @param word
+     * @return
+     */
     public Integer indexNode(ArrayList<T> word) {
         TrieNode<T> current = root;
         for (int i = 0; i < word.size(); i++) {
