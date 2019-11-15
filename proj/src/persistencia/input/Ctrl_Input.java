09f2a6459d8b4a0b887e554c16d393acb2842d63
mipaoc991 <miguelparacuellosocana@gmail.com>
2019-11-15 02:33:20 +0100

    Doxygen done

--- proj/src/persistencia/input/Ctrl_Input.java
+++ proj/src/persistencia/input/Ctrl_Input.java
@@ -1,3 +1,8 @@
+/**
+ * @class Ctrl_Input
+ * @brief Classe de Ctrl_Input
+ * @author 
+ */
 package persistencia.input;
 
 import java.util.regex.Pattern;
@@ -5,12 +10,21 @@ import java.util.regex.Pattern;
 public class Ctrl_Input {
 
     //Attributes
+    /**
+     * @param Input_class Classe Input
+     */
     protected Input Input_class;
 
+    /**
+     * @param Extensio Extensio de l'arxiu que estem tractant
+     */
     String Extensio;
 
 
-    //Constructor
+    /**
+     * @brief Constructor de la classe
+     * @param path Path de l'arxiu a tractar
+     */
     public Ctrl_Input(String path) {
   
         Input_class = new Input(path);
@@ -24,19 +38,35 @@ public class Ctrl_Input {
     }
     
     //Function
+    /**
+     * @fn public String getExtension()
+     * @return Retorna l'extensio de l'arxiu
+     */
     public String getExtension() {
         return Extensio;
     }
 
+    /**
+     * @fn public boolean finished()
+     * @return Retorna si hem acabat de llegir l'arxiu o no
+     */
     public boolean finished() {
         return Input_class.finished();
     }
 
+    /**
+     * @fn public String getAlg()
+     * @return Proporciona el algoritme més adient per tractar la descompressio
+     */
     public String getAlg()
     {
         return Input_class.getDecodeAlg();
     }
 
+    /**
+     * @fn public void getMetadata()
+     * @brief Llegeix el dos primers bits de l'arxiu comprimit per saber amb quin algoritme ha sigut tractat
+     */
     public void getMetadata() {
         Input_class.getBits(2);
     }
--- proj/src/persistencia/input/Ctrl_Input_Text.java
+++ proj/src/persistencia/input/Ctrl_Input_Text.java
@@ -1,16 +1,32 @@
+/**
+ * @class Ctrl_Input_Text
+ * @brief Classe de Ctrl_Input_Text
+ * @author Miguel Paracuellos
+ */
 package persistencia.input;
-
 
 public class Ctrl_Input_Text extends Ctrl_Input {
 
+    /**
+     * @param seguent Proper byte a llegir
+     */
     byte seguent;
 
+    /**
+     * @brief Constructor de la classe
+     * @param path Path de l'arxiu input
+     */
     public Ctrl_Input_Text(String path) {
         super(path);
         seguent = Input_class.getBits(8);
     }
  
-    //Passa tots els carÃ cters a codi ASCII
+    /**
+     * @fn public byte get()
+     * @brief Proporciona el próxim byte a llegir
+     * @note Passa tots els caracters a codi ASCII
+     * @return Retorna el pròxim byte de l'arxiu, en el cas de que no haguem arribat al final
+     */
     public byte get() {
         byte actual = seguent;
         seguent = Input_class.getBits(8); 
