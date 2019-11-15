bdd5f39e8afd1b19b4efdbe0f5f9b7aa7a4f7f41
Joan <joan.bellavista.bartroli@est.fib.upc.edu>
2019-11-15 21:34:59 +0100

    ****Nou instalador****

--- proj/src/compileAndRun.sh
+++ proj/src/compileAndRun.sh
@@ -1,13 +1,23 @@
 
-if [[ $1 != "-remove" && $1 != "-clean" && $1 != "-driver" ]]; then
+if [[ $# -eq 0 ]]; then
+    echo "Compilem els seguents arxius: "
     ArxiusAcompilar=$(find | grep .java | tr "\n" " ")
     echo $ArxiusAcompilar | sed "s/ /\n/g"
     javac $ArxiusAcompilar -d ../bin
-    cp -r ../bin/* ../'drivers y tests exe'
-fi
-if [[ $1 == "-driver" ]]; then
-    cd ../'drivers y tests exe'
+elif [[ $1 == "-driver" ]]; then
+    echo "Executem driver"
+    cd ../bin
     java $2
+    cd ../src
+elif [[ $1 == "-ctest" ]]; then 
+    echo "Compilem tests"
+    TestsAcompilar=$(find | grep .java | sed "/.*Test\./! d" | tr "\n" " ")
+    echo $TestsAcompilar | sed "s/ /\n/g"
+    javac -cp .:junit.jar:hamcrest-core-1.3.jar $TestsAcompilar -d ../bin
+elif [[ $1 == "-etest" ]]; then
+    echo "Executem tests"
+    cd ../bin
+    java -cp .:junit.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore $2
     cd ../src
 fi
 
