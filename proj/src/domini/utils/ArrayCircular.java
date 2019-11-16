700bde32cf7269b27048afae9381c83d1e67c061
Manel <manelaguilar@outlook.com>
2019-11-16 23:01:19 +0100

    modificació doxygen

--- proj/src/domini/utils/ArrayCircular.java
+++ proj/src/domini/utils/ArrayCircular.java
@@ -3,12 +3,35 @@ package domini.utils;
 
 public class ArrayCircular {
 
+    /**
+     * @param start punter a la primera posició disponible.
+     */
     private int start;
+
+    /**
+     * @param end punter a la última posició.
+     */
     private int end;
+
+    /**
+     * @param size mida del atribut array.
+     */
     private int size;
+
+    /**
+     * @param array vector de bytes que contindrà la informació.
+     */
     private byte[] array;
+
+    /**
+     * @param afegits nombre de valors afegits al atribut array.
+     */
     private int afegits;
     
+    /**
+     * @brief El constructor.
+     * @param mida inicialitza el atribut array amb mida mida.
+     */
     public ArrayCircular(int mida)
     {
         size = mida;
@@ -18,6 +41,11 @@ public class ArrayCircular {
         array = new byte[mida];
     }
 
+    /**
+     * @fn public void setValue(byte value)
+     * @brief Mètode per assignar el valor value al atribut array a la posició start.
+     * @param value
+     */
     public void setValue(byte value)
     {
         array[start] = value;
@@ -26,12 +54,23 @@ public class ArrayCircular {
         if(afegits == size) end = (++end)%size;
     }
 
+    /**
+     * @fn public byte getValue(int index)
+     * @param index
+     * @return Retorna el byte a la posició index de array
+     */
     public byte getValue(int index)
     {
         if(index < 0 || index >= size) throw new IndexOutOfBoundsException("Fora de rang");
         return array[index];
     }
 
+    /**
+     * @fn public byte getValueAmbDespl(int despl)
+     * @brief dóna el byte de despl vegades cap enrere (des de start).
+     * @param despl
+     * @return Retorna el byte a la posició indicada del array
+     */
     public byte getValueAmbDespl(int despl)
     {
         int ret = start - despl;
@@ -44,16 +83,32 @@ public class ArrayCircular {
         return array[ret];
     }
 
+    /**
+     * @fn public int getStart()
+     * @brief dóna el int de la primera posició vàlida del array.
+     * @return Retorna l'atribut start
+     */
     public int getStart()
     {
         return start;
     }
 
+    /**
+     * @fn public int getEnd()
+     * @brief dóna el int de la última posició vàlida del array.
+     * @return Retorna l'atribut end
+     */
     public int getEnd()
     {
         return end;
     }
 
+    /**
+     * @fn public boolean isIn(byte value)
+     * @brief Recorregut lineal sobre l'atribut array per trobar value.
+     * @param value
+     * @return true si value està a array else false.
+     */
     public boolean isIn(byte value)
     {
         for(int i = 0; i < afegits; i++)
@@ -61,6 +116,10 @@ public class ArrayCircular {
         return false;
     }
     
+    /**
+     * @fn public int getAfegits()
+     * @return retorna el atribut privat afegits.
+     */
     public int getAfegits()
     {
         return afegits;
