ae33d4b2c03ae806c992d47bf402068712b84f36
Seisman99 <55874070+Seisman99@users.noreply.github.com>
2019-11-18 13:49:32 +0100

    Update Estadistica.java

--- src/domini/estadistica/Estadistica.java
+++ src/domini/estadistica/Estadistica.java
@@ -60,6 +60,7 @@ public class Estadistica {
             if (ts == 0) ts = 1;
             long spd = input.length() / ts;
             double gc = (double)input.length() / (double)output.length();
+            if(input.length() == output.length()) gc = 1;
 
             System.out.println("La velocitat de compressió ha sigut de " + spd + " bytes/ms.");
             System.out.println("El grau de compressió és de " + String.format("%.6f", gc));
