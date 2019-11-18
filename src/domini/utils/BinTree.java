7401c81f5004e9726869564c06eb266e6f57df67
jbb494 <49374692+jbb494@users.noreply.github.com>
2019-11-18 12:49:47 +0100

    Update BinTree.java

--- src/domini/utils/BinTree.java
+++ src/domini/utils/BinTree.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 /**
  * @class BinTree
  * @brief Permet expressar arbres binaris on les fulles poden prendre valors enters.
- * Els nodes s'identifiquen amb enters.
- * Aquesta estroctura de dades permet afegir informació però no permet 
+ * Els nodes s'identifiquen amb enters. Aquesta estroctura de dades permet afegir informació però no permet
  * eliminar o modificar informació prèviament afegida,
  * @author Joan Lapeyra Amat
  */
