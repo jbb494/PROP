683f456a6e2b8dafbc9d006be037933606c458ad
jlapeyra <39958970+jlapeyra@users.noreply.github.com>
2019-12-16 20:12:41 +0100

    Faltava el que vaig pujar ahir a master

--- src/domini/folders/Ctrl_FolderFile.java
+++ src/domini/folders/Ctrl_FolderFile.java
@@ -352,29 +352,45 @@ public class Ctrl_FolderFile {
      * @return retorna true <=> el path que s'ha passat al constructor és a un comprimit.
      */
     public boolean isEncoded() {
+        if (!FileNames.getExtension(path_in).equals("jm")) return false;
         Ctrl_Input_Encoded in = new Ctrl_Input_Encoded(path_in);
         byte magic_word = in.getByte();
-        return (magic_word == 0xF0) || (magic_word == 0xF1);
+        return (magic_word == (byte)0xF0) || (magic_word == (byte)0xF1);
     }
 
     /**
-     * @fn public boolean isEncoded()
+     * @fn public boolean isEncodedFolder()
      * @return retorna true <=> el path que s'ha passat al constructor és a una carpeta comprimida.
      */
     public boolean isEncodedFolder() {
+        if (!FileNames.getExtension(path_in).equals("jm")) return false;
         Ctrl_Input_Encoded in = new Ctrl_Input_Encoded(path_in);
         byte magic_word = in.getByte();
-        return (magic_word == 0xF0);
+        return (magic_word == (byte)0xF0);
     }
 
     /**
-     * @fn public boolean isEncoded()
+     * @fn public boolean isEncodedFolder()
      * @return retorna true <=> el path que s'ha passat al constructor és a un fitxer comprimit.
      */
     public boolean isEncodedFile() {
+        if (!FileNames.getExtension(path_in).equals("jm")) return false;
         Ctrl_Input_Encoded in = new Ctrl_Input_Encoded(path_in);
         byte magic_word = in.getByte();
-        return (magic_word == 0xF1);
+        return (magic_word == (byte)0xF1);
+    }
+
+    /**
+     * @fn public String getEncodedExtension()
+     * @return L'extensió que tenia un comprimit abans de ser comprimit.
+     */
+    public String getEncodedExtension() {
+        if (!FileNames.getExtension(path_in).equals("jm")) throw new IllegalArgumentException("No es un comprimit.");
+        Ctrl_Input_Encoded in = new Ctrl_Input_Encoded(path_in);
+        byte magic_word = in.getByte();
+        if (magic_word == (byte)0xF1) return in.getWord();
+        if (magic_word == (byte)0xF0) return "";
+        throw new IllegalArgumentException("No es un comprimit.");
     }
 
     public String getFile(String pathTemp) throws IOException {
