a17a9bd31ff07dc7915b8087c25e571b16a10364
Manel <manelaguilar@outlook.com>
2019-12-17 20:37:03 +0100

    mes merda doxygen

--- src/global/global.java
+++ src/global/global.java
@@ -1,25 +1,32 @@
 package global;
 
 public abstract class global {
+
     public enum type{
-        folder,
-        text,
-        image,
-        comprimit,
-        imageComprimit,
-        textComprimit,
-        folderComprimit
+        folder, ///< Tipo Carpeta
+        text, ///< Tipo Texto
+        image, ///< Tipo Imagen
+        comprimit, ///< Tipo Comprimido
+        imageComprimit, ///< Tipo Imagen Comprimida
+        textComprimit, ///< Tipo Texto Comprimido
+        folderComprimit ///<Tipo Carpeta Comprimida
     }
 
     public enum method {
-        lz78,
-        lzw,
-        lzss,
-        jpeg
+        lz78, ///< Tipo de algoritmo LZ78
+        lzw,///< Tipo de algoritmo LZW
+        lzss, ///< Tipo de algoritmo LZSS
+        jpeg ///< Tipo de algoritmo JPEG
     }
     public enum jpegCompression {
-        low,
-        medium,
-        high
+        low, ///< Grado de Compresión JPEG bajo
+        medium, ///< Grado de Compresión JPEG medio
+        high ///< Grado de Compresión JPEG alto
     }
 }
+
+/** @class global
+ *  @brief Clase que contiene diferentes tipos de enumeration.
+ *
+ *  @author
+ */
